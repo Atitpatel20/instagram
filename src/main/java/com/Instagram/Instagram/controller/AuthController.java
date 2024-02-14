d0b5be1f933cd6ac8b25fb0b4af80b8dce8c65e7
Atit Patel <atitpatel296@gmail.com>
2024-02-14 22:12:48 +0530

    127:developed sign in feature with spring security 09.02.2024

--- src/main/java/com/Instagram/Instagram/controller/AuthController.java
+++ src/main/java/com/Instagram/Instagram/controller/AuthController.java
@@ -2,12 +2,17 @@ package com.Instagram.Instagram.controller;
 
 import com.Instagram.Instagram.entity.Role;
 import com.Instagram.Instagram.entity.User;
+import com.Instagram.Instagram.payload.LoginDto;
 import com.Instagram.Instagram.payload.SignUpDto;
 import com.Instagram.Instagram.repository.RoleRepository;
 import com.Instagram.Instagram.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,7 +33,19 @@ public class AuthController {
     @Autowired
     private RoleRepository roleRepository;
 
-    @PostMapping("/signUp")
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/signin")
+    public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail(), loginDto.getPassword());
+        Authentication authenticate = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
+    }
+
+        @PostMapping("/signUp")
     public ResponseEntity<?> createRegistration(@RequestBody SignUpDto signUpDto) {
 
         if(userRepository.existsByEmail(signUpDto.getEmail())){
